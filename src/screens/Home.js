92a902f849e9fb9c740c9bbec48746fc1cf98628
armand <armandmiron123@gmail.com>
2023-05-15 19:39:32 +0300

    da

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,38 @@
 import { Text, View } from 'react-native';
-import { Button } from 'react-native-paper';
-  
+import { Button, Title } from 'react-native-paper';
+import { IconButton, Colors } from 'react-native-paper';
+import TitleLogo from '../../assets/title.svg';
+import {Card} from 'react-native-paper';
+import Lung_Img from '../../assets/lungs.svg';
+function Lungs() {
+    return (
+        <View>
+            <Card>
+    <Card.Title title="Card Title" subtitle="Card Subtitle"/>
+    <Card.Content>
+      <Title>Card title</Title>
+    </Card.Content>
+    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+    <Card.Actions>
+      <Button>Cancel</Button>
+      <Button>Ok</Button>
+    </Card.Actions>
+  </Card>
+        </View>
+    );
+}
+
 export default function Home({navigation}) {
     return (
             <View>
-            <Button icon = 'arrow-u-left-top-bold' mode='elevated' onPress={()=>navigation.navigate("Intro")}
-            style={{
-                alignSelf: 'flex-start',
-                
-            }}/>
+                <TitleLogo style={{
+                alignSelf:'center',
+                justifyContent:'center',
+             }} width={200} height={200}>
+                <IconButton icon = 'arrow-u-left-top-bold' 
+                        mode='contained' onPress={()=>navigation.navigate("Intro")}/>
+             </TitleLogo>
+             <Lungs/>
             </View>
     );
 }
--- src/screens/Intro.js
+++ src/screens/Intro.js
@@ -1,19 +1,24 @@
 import { Text, View } from 'react-native';
-import { Button } from 'react-native-paper';
+import { Button,IconButton } from 'react-native-paper';
+import Logo from '../../assets/logo.svg';
+import Start from '../../assets/start_button.svg';
+
 
 export default function Intro({navigation}) {
     return (
-        <View style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '60%',
-            textAlign: 'center',
-            fontSize: 25}}>
-            <Button onPress={()=>navigation.navigate("Home")}>
-                Startdsada
-            </Button>
-        </View>
+        <View style={{flex: 1 , backgroundColor: 'white'}}>
+             <Logo style={{
+                alignSelf:'center',
+                justifyContent:'center',
+             }} width={300} height={600} />
+             <IconButton icon = {Start} style={{
+                alignSelf: 'center',
+                width:200,
+                height:80,
+                backgroundColor: 'transparent'
+            }}
+            mode='contained' onPress={()=>navigation.navigate("Home")}/>
+            </View>
+            
     );
 }

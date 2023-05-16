5b0b9346907a9484a16a1548c9286aa67444ca13
armand <armandmiron123@gmail.com>
2023-05-16 15:13:11 +0300

    Login System

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,23 +1,13 @@
-import { ImageBase, View } from 'react-native';
-import {Text} from 'react-native-paper';
-import { Button, DefaultTheme, Paragraph, Title } from 'react-native-paper';
-import { IconButton, Colors , Card} from 'react-native-paper';
-import TitleLogo from '../../assets/title.svg';
-import Lung_Img from '../../assets/lungs.svg';
-import { Avatar } from 'react-native-paper';
-import Animatiodan from '../../assets/animation'
-import NavBar from '../../components/navigation_bar'
+import { View } from 'react-native';
+import { Button,DefaultTheme,IconButton,Text } from 'react-native-paper';
 export default function Home({navigation}) {
     return (
-            <View>
-                <TitleLogo style={{
-                alignSelf:'center',
-                justifyContent:'center',
-             }} width={200} height={170}>
-                <IconButton icon = 'arrow-u-left-top-bold' 
-                        mode='contained' onPress={()=>navigation.navigate("Intro")}/>
-             </TitleLogo>
-             <NavBar/>
+        <View>
+                <Text variant='headlineLarge' theme={DefaultTheme} style={{
+                    alignSelf: 'center',
+                    paddingVertical: 50,
+                    fontFamily: 'Arial',
+                }}>Home</Text>
             </View>
     );
 }
